bb1b36200a3183b284e3abc00fc36e92fbd264b2
Artem Chymshyr <artem.chymshyr@gmail.com>
2025-02-03 20:05:02 +0100

    fixed version of controllers

--- src/main/java/org/example/forum/controllers/LoginController.java
+++ src/main/java/org/example/forum/controllers/LoginController.java
@@ -2,8 +2,11 @@ package org.example.forum.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("/login")
+@Controller
+@RequestMapping("/login")
 public class LoginController {
 
     @GetMapping
--- src/main/java/org/example/forum/controllers/RegistrationController.java
+++ src/main/java/org/example/forum/controllers/RegistrationController.java
@@ -2,8 +2,10 @@ package org.example.forum.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("/register")
+@Controller
+@RequestMapping("/register")
 public class RegistrationController {
 
     @GetMapping
