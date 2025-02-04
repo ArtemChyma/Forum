eae7e74bb289db8a95f8a3a08ba184ea5b0d56fc
Artem Chymshyr <artem.chymshyr@gmail.com>
2025-02-04 17:02:14 +0100

    SecurityConfig was made for an appropriate view of Login and Registration pages as well been rectified those files with addition of a button Log In and Register respectively

---
+++ src/main/java/org/example/forum/configs/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package org.example.forum.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(req -> req.requestMatchers("/login", "/register")
+                .permitAll().anyRequest().authenticated())
+                .formLogin(f -> f.loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/main_page", true).permitAll()
+                ).logout(l -> l.invalidateHttpSession(true)
+                        .clearAuthentication(true).logoutRequestMatcher(
+                                new AntPathRequestMatcher(("logout"))
+                        ).logoutSuccessUrl("/login?logout").permitAll());
+        return http.build();
+    }
+}
