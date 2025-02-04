71cfb6ea083f37c3b12e1c82cc65b2ec12662593
Artem Chymshyr <artem.chymshyr@gmail.com>
2025-02-04 18:02:41 +0100

    user repository added to manage queries to the database

---
+++ src/main/java/org/example/forum/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package org.example.forum.repositories;
+
+import org.example.forum.Entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<> {
+
+}
