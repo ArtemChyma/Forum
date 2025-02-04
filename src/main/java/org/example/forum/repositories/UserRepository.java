8f7b7b018f927b7f4dd470848ce58f81bbc48b7a
Artem Chymshyr <artem.chymshyr@gmail.com>
2025-02-04 18:13:01 +0100

    updated UserRepository

--- src/main/java/org/example/forum/repositories/UserRepository.java
+++ src/main/java/org/example/forum/repositories/UserRepository.java
@@ -3,6 +3,6 @@ package org.example.forum.repositories;
 import org.example.forum.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<> {
+public interface UserRepository extends JpaRepository<Long, User> {
 
 }
