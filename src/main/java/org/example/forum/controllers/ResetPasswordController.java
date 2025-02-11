9da263dac09695edfa1b4f79f7cb4823e345836f
Artem Chymshyr <artem.chymshyr@gmail.com>
2025-02-11 17:48:48 +0100

    added new dependency allowing validate data, EmailValidator and work is being done on resetting password controller

--- src/main/java/org/example/forum/controllers/ResetPasswordController.java
+++ src/main/java/org/example/forum/controllers/ResetPasswordController.java
@@ -3,11 +3,15 @@ package org.example.forum.controllers;
 import lombok.RequiredArgsConstructor;
 import org.example.forum.repositories.UserRepository;
 import org.example.forum.services.MailSenderService;
+import org.example.forum.validators.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/reset")
@@ -28,5 +32,11 @@ public class ResetPasswordController {
         return "reset-password-stage1";
     }
 
-
+    @PostMapping
+    public String sendEmailTo(Model model) {
+        String email = (String) model.getAttribute("email");
+        if (EmailValidator.isValidEmail(email)) {
+            mailSenderService.send(email, "Password recovery link", "");
+        }
+    }
 }
