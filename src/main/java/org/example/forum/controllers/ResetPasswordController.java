3d379f9dfdd9746803a0a72feb6a08018ae3ccfb
Artem Chymshyr <artem.chymshyr@gmail.com>
2025-02-11 19:34:38 +0100

    token repository and some get method to change password were added

--- src/main/java/org/example/forum/controllers/ResetPasswordController.java
+++ src/main/java/org/example/forum/controllers/ResetPasswordController.java
@@ -2,6 +2,8 @@ package org.example.forum.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.example.forum.Entities.Token;
+import org.example.forum.repositories.TokenRepository;
 import org.example.forum.repositories.UserRepository;
 import org.example.forum.services.MailSenderService;
 import org.example.forum.validators.EmailValidator;
@@ -12,6 +14,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -21,16 +25,34 @@ public class ResetPasswordController {
 
 
     private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
     private final MailSenderService mailSenderService;
-
     private final String DEFAULT_LINK_TO_RECOVERY = "http://localhost:8080/reset/";
-    private String generateToken() {
-        return UUID.randomUUID().toString();
-    }
+    private final int TIME_TO_EXPIRE = 120000;
+
     @Autowired
-    public ResetPasswordController(UserRepository userRepository, MailSenderService mailSenderService) {
+    public ResetPasswordController(UserRepository userRepository, MailSenderService mailSenderService,
+                                   TokenRepository tokenRepository) {
         this.userRepository = userRepository;
         this.mailSenderService = mailSenderService;
+        this.tokenRepository = tokenRepository;
+    }
+    private String generateToken() {
+        String generatedToken = UUID.randomUUID().toString();
+
+        Token token = new Token();
+        token.setToken(generatedToken);
+        token.setUsed("NOT_USED");
+
+        Date currentDate = new Date();
+        Timestamp creationDate = new Timestamp(currentDate.getTime());
+        token.setCreated_at(creationDate);
+
+        Timestamp expirationDate = new Timestamp(currentDate.getTime() + TIME_TO_EXPIRE);
+        token.setExpires_at(expirationDate);
+
+        tokenRepository.save(token);
+        return generatedToken;
     }
 
     @GetMapping()
@@ -51,6 +73,13 @@ public class ResetPasswordController {
     @GetMapping("/{token}")
     public String resetPassword(Model model, @PathVariable("token") String token) {
         model.addAttribute("token", token);
+        Optional<Token> user_token = tokenRepository.findTokenByToken(token);
+        if (user_token.isEmpty())
+            throw new RuntimeException("TOKEN NOT EXISTS");
+        if (user_token.get().getUsed().equals("USED"))
+            return "token-used";
+        user_token.get().setUsed("USED");
+        tokenRepository.save(user_token.get());
         return "reset-password-stage2";
     }
 }
