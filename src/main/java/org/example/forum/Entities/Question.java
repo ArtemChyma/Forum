1b862a813fd8e94be4f2e7bf8fe1edbe2b3b6e82
Artem Chymshyr <artem.chymshyr@gmail.com>
2025-03-17 22:04:09 +0100

    question additions is ready with photos of the problem storage at the local machine

--- src/main/java/org/example/forum/Entities/Question.java
+++ src/main/java/org/example/forum/Entities/Question.java
@@ -19,4 +19,81 @@ public class Question {
     private Integer views;
     private String userImage;
     private String questionImages;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getQuestionContent() {
+        return questionContent;
+    }
+
+    public void setQuestionContent(String questionContent) {
+        this.questionContent = questionContent;
+    }
+
+    public Timestamp getPostTime() {
+        return postTime;
+    }
+
+    public void setPostTime(Timestamp postTime) {
+        this.postTime = postTime;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public Integer getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(Integer answers) {
+        this.answers = answers;
+    }
+
+    public Integer getViews() {
+        return views;
+    }
+
+    public void setViews(Integer views) {
+        this.views = views;
+    }
+
+    public String getUserImage() {
+        return userImage;
+    }
+
+    public void setUserImage(String userImage) {
+        this.userImage = userImage;
+    }
+
+    public String getQuestionImages() {
+        return questionImages;
+    }
+
+    public void setQuestionImages(String questionImages) {
+        this.questionImages = questionImages;
+    }
 }
--- src/main/java/org/example/forum/controllers/QuestionController.java
+++ src/main/java/org/example/forum/controllers/QuestionController.java
@@ -1,16 +1,70 @@
 package org.example.forum.controllers;
 
+import org.example.forum.Entities.Question;
+import org.example.forum.repositories.QuestionRepository;
+import org.example.forum.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.Principal;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/question/")
 public class QuestionController {
-
+    private final UserRepository userRepository;
+    private final QuestionRepository questionRepository;
+    @Value("${app.storage.path}")
+    private String FILES_STORAGE_PATH;
+    @Autowired
+    public QuestionController(UserRepository userRepository, QuestionRepository questionRepository) {
+        this.userRepository = userRepository;
+        this.questionRepository = questionRepository;
+    }
     @GetMapping("/ask")
-    public String postQuestion() {
+    public String postQuestion(Model model) {
+        model.addAttribute("question", new Question());
         return "ask-question";
+    }
+
+    @PostMapping("/ask")
+    public String processQuestion(Principal principal, @RequestParam("question-images")MultipartFile[] files, @ModelAttribute("question") Question question) {
+        if (files != null && files.length != 0) {
+            if (userRepository.findUserByEmail(principal.getName()).isPresent()) {
+                try {
+                    Path uploadPath = Paths.get(FILES_STORAGE_PATH + "users/" + userRepository.findUserByEmail(principal.getName()).get().getId() + "/questions/" + questionRepository.count() + "/images" + "/");
+                    if (!Files.exists(uploadPath)) {
+                        Files.createDirectories(uploadPath);
+                    }
+                    for (MultipartFile file : files) {
+                        if (!file.isEmpty()) {
+                            Path filePath = uploadPath.resolve(file.getOriginalFilename());
+                            Files.copy(file.getInputStream(), filePath);
+                        }
+                    }
+                    question.setAnswers(0);
+                    question.setViews(0);
+                    question.setUserId(userRepository.findUserByEmail(principal.getName()).get().getId());
+                    question.setQuestionImages(questionRepository.count() + "/images" + "/");
+                    question.setPostTime(new Timestamp(System.currentTimeMillis()));
+                    questionRepository.save(question);
+                }catch(IOException e){
+                    e.printStackTrace();
+                    System.out.println("Error multipart file happened");
+                }
+            }
+        }
+        return "main-page";
     }
 
 }
